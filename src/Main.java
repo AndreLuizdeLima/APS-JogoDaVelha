2a33e0f9dca6cb6b2983d56aa8d26d1bf109c5cd
Andre <andreluizjp2015@gmail.com>
2025-05-26 20:03:02 -0300

    add imprimiTabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import static ferramentas.ImprimeTabuleiro.imprimeTabuleiro;
 import static jogo.Usuario.escolhaSimbulo;
 import static jogo.Usuario.setarJogador;
 
 public class Main {
     public static void main(String[] args) {
+        char[] tabuleiro = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         setarJogador();
         escolhaSimbulo();
+        imprimeTabuleiro(tabuleiro);
     }
 }
--- src/ferramentas/ImprimeTabuleiro.java
+++ src/ferramentas/ImprimeTabuleiro.java
@@ -1,4 +1,28 @@
 package ferramentas;
 
 public class ImprimeTabuleiro {
+
+
+
+     public static void imprimeTabuleiro(char[] tabuleiro){
+        char[] posicoes = tabuleiro;
+
+        System.out.print("""
+             1   2   3
+           +---+---+---+
+        A  | %s | %s | %s |
+           +---+---+---+
+        B  | %s | %s | %s |
+           +---+---+---+
+        C  | %s | %s | %s |
+           +---+---+---+
+        """.formatted(
+                posicoes[0], posicoes[1], posicoes[2],
+                posicoes[3], posicoes[4], posicoes[5],
+                posicoes[6], posicoes[7], posicoes[8]
+        ));
+    }
+
+
 }
+
