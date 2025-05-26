fc3cc2817d70fc7962ccfd49cd8f50e4d7328a27
Andre <andreluizjp2015@gmail.com>
2025-05-26 20:54:38 -0300

    atualizando maquina

--- src/ferramentas/Maquina.java
+++ src/ferramentas/Maquina.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-class Maquina {
-    public static int[] maquina01(char[][] tabuleiro) {
-        List<int[]> campoValido = new ArrayList<>();
+public class Maquina{
+    public static int maquina01(char[] tabuleiro) {
+        List<Integer> campoValido = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (tabuleiro[i][j] == ' ') {
-                    campoValido.add(new int[]{i, j});
-                }
+        for (int i = 0; i < tabuleiro.length; i++) {
+            if (tabuleiro[i] == ' ') {
+                campoValido.add(i);
             }
         }
 
         if (campoValido.isEmpty()) {
-            return null;
+            return -1;
         }
 
         Random rd = new Random();
         return campoValido.get(rd.nextInt(campoValido.size()));
     }
+
 }
