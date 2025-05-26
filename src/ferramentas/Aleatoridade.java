ba35dd6e5867a733a52b1cec4e30ffdbe0c70e97
Ulisses <dev.ulissesneto@gmail.com>
2025-05-26 20:22:47 -0300

    Comportamento da maquina modo facil, escolhendo aleatoriamente os campos

--- src/ferramentas/Aleatoridade.java
+++ src/ferramentas/Aleatoridade.java
@@ -9,3 +9,28 @@ public class Aleatoridade {
         return num;
     }
 }
+//Comportamento da maquina modo facil, escolhendo aleatoriamente os campos
+
+class Maquina {
+    public static int[] maquina01(char[][] tabuleiro) {
+        List<int[]> campoValido = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tabuleiro[i][j] == ' ') {
+                    campoValido.add(new int[]{i, j});
+                }
+            }
+        }
+
+
+        if (campoValido.isEmpty()) {
+            return null;
+        }
+
+
+        Random rd = new Random();
+        return campoValido.get(rd.nextInt(campoValido.size()));
+    }
+}
+
